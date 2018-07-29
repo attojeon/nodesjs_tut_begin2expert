6abe616735aa34107f48f6e27595b7608b61fdc0
attojeon <atto.jeon@gmail.com>
2018-07-29 15:58:14 +0900

    Update config.js

--- 41-server.js/server/config/config.js
+++ 41-server.js/server/config/config.js
@@ -1,5 +1,5 @@
 var dbuser = 'dbowner';
-var passwd = 'Yoon0819';
+var passwd = '';
 process.env.PORT = 3000;
 process.env.MONGDB_URI = `mongodb://${dbuser}:${passwd}@ds249311.mlab.com:49311/tododb`;
 DBCONN_STRING = `mongodb://${dbuser}:${passwd}@ds249311.mlab.com:49311/tododb`;
